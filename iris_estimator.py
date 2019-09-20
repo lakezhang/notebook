4099bf4c6ac11d345968d4a9d229da0ae3e698dd
lakezhang <151828515@qq.com>
2019-09-20 15:41:49 +0800

    Update iris_estimator.py
    add prediction

--- iris_estimator.py
+++ iris_estimator.py
@@ -8,17 +8,23 @@ SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 CSV_TYPES = [[0.0], [0.0], [0.0], [0.0], [0]]
 
 
-def decode_line(line) :
+def decode_line(line, need_label = True) :
+    '''
+        NO need_label when predict
+    '''
     items = tf.decode_csv(line, CSV_TYPES, ',')
 
     features = dict(zip(COLUMN_NAMES, items))
     label = features.pop('Species')
 
+    if not need_label :
+        return features
+
     return features, label
 
-def get_input_fn(file_name, batch_size = 32, is_shuffle = False, epochs=0) :
+def get_input_fn(file_name, batch_size = 32, is_shuffle = False, epochs=0, label = True) :
     dataset = tf.data.TextLineDataset(file_name).skip(1)
-    dataset = dataset.map(decode_line)
+    dataset = dataset.map(lambda x : decode_line(x, label))
 
     if epochs > 0:
         dataset = dataset.repeat(epochs)
@@ -41,13 +47,24 @@ def main() :
 
     steps = int(120 * 3 / 32)
 
-    classifier.train(input_fn = lambda : get_input_fn("/Users/zhangzeming/codes/github/dataset/iris/iris_training.csv"
+    classifier.train(input_fn = lambda : get_input_fn("../dataset/iris/iris_training.csv"
         , epochs = 3), steps = steps)
 
     eval_result = classifier.evaluate(
-        input_fn = lambda : get_input_fn("/Users/zhangzeming/codes/github/dataset/iris/iris_test.csv"))
+        input_fn = lambda : get_input_fn("../dataset/iris/iris_test.csv"))
 
     print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+    predictions = classifier.predict(
+        input_fn = lambda : get_input_fn("../dataset/iris/iris_test.csv", batch_size = 10, label=False))
+
+    template = ('Prediction is "{}" ({:.1f}%)')
+
+    for pred_dict in predictions :
+        class_id = pred_dict['class_ids'][0]
+        probability = pred_dict['probabilities'][class_id]
+
+        print(template.format(SPECIES[class_id], 100 * probability))
 
 if __name__ == '__main__':
     main()
